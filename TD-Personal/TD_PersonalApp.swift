41ebc47e54891cb2dc1164b1e3a998eaf595c920
Moises Sanchez <moy.sav21@hotmail.com>
2024-12-03 22:37:40 -0600

    Initial Commit

---
+++ TD-Personal/TD_PersonalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TD_PersonalApp.swift
+//  TD-Personal
+//
+//  Created by Moises Sanchez on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TD_PersonalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
